7815e2fceff2399afe9417e5515b8cd18a4e30a8
Jordan Ginor <jordan.ginor@gmail.com>
2019-01-01 18:46:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 
 def speak_to_grandma(argument)
-  if argument == argument.upcase
-    "NO, NOT SINCE 1938!"
-  elsif argument == "I LOVE YOU GRANDMA!"
+  if argument == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
+  elsif argument == argument.upcase
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
